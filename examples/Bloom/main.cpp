9ad29b0e2b0f9a9f3bca46c6ff3c76233d7df073
ToyAuthor <eye5002003@gmail.com>
2015-05-23 17:35:23 +0800

    原來只有一點點漏掉而已，Bloom可以正確秀模糊效果了(Linux還不知道)

--- examples/Bloom/main.cpp
+++ examples/Bloom/main.cpp
@@ -6,6 +6,7 @@
 
 int main()
 {
+	glViewport(0, 0, 512, 512);
 	GutCreateWindow(100, 100, 512, 512, "HelloOpenGL");
 
 
@@ -14,6 +15,20 @@ int main()
 		ReleaseResourceOpenGL();
 		printf("Failed to load resources\n");
 		exit(0);
+	}
+
+	int i;
+	const int num_samples = KERNELSIZE;
+
+	float sum = 0.0f;
+	for ( i=0; i<num_samples; i++ )
+	{
+		sum += g_weight_table[i];
+	}
+
+	for ( i=0; i<num_samples; i++ )
+	{
+		g_weight_table[i] /= sum;
 	}
 
 	while( GutProcessMessage() )
